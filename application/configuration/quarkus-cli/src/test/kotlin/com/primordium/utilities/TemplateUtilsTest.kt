270a6929d92b03f21b8c80ef78d4063d1a6ec509
maartenvandeperre <maarten.vandeperre@viae.solutions>
2024-04-11 10:02:28 +0200

    tests are fixed

--- application/configuration/quarkus-cli/src/test/kotlin/com/primordium/utilities/TemplateUtilsTest.kt
+++ application/configuration/quarkus-cli/src/test/kotlin/com/primordium/utilities/TemplateUtilsTest.kt
@@ -1,5 +1,6 @@
 package com.primordium.utilities
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.primordium.configuration.quarkuscli.utils.TemplateUtils
 import com.primordium.core.coreutils.functional.ErrorResponse
 import com.primordium.core.coreutils.functional.SuccessResponse
@@ -25,26 +26,83 @@ open class TemplateUtilsTest {
             fail("No error response expected: ${result.errorMessages.joinToString(";")}")
         } else {
             val templateResult = (result as SuccessResponse<Template>).data
-            assertThat(templateResult).isEqualTo(
-                Template(
-                    name = "Primordium Template",
-                    version = "0.0.1",
-                    fields = listOf(
-                        TemplateField(
-                            name = "application-checks",
-                            subFields = listOf(
-                                TemplateField(
-                                    name = "heart-beat-check",
-                                    value = "a heart beat check implementation"
-                                ),
-                                TemplateField(
-                                    name = "conscious-check",
-                                    value = "a consciousness check implementation"
-                                )
-                            )
-                        )
-                    )
-                )
+            assertThat(ObjectMapper().writeValueAsString(templateResult)).isEqualTo(
+                """
+                    {
+                       "name":"Primordium Application",
+                       "version":"0.0.1",
+                       "fields":[
+                          {
+                             "name":"metadata",
+                             "value":null,
+                             "subFields":[
+                                {
+                                   "name":"name",
+                                   "value":"primordium-application",
+                                   "subFields":[
+                                      
+                                   ]
+                                },
+                                {
+                                   "name":"namespace",
+                                   "value":"primordium-project",
+                                   "subFields":[
+                                      
+                                   ]
+                                },
+                                {
+                                   "name":"port",
+                                   "value":"8080",
+                                   "subFields":[
+                                      
+                                   ]
+                                },
+                                {
+                                   "name":"instance-count",
+                                   "value":"2",
+                                   "subFields":[
+                                      
+                                   ]
+                                },
+                                {
+                                   "name":"image",
+                                   "value":"image-registry.openshift-image-registry.svc:5000/webinar/person-service@sha256:a89c47d43f1f7aa527bffce3bb5c92d63be2211f5ec93b47cd5cb0f456c7ac05",
+                                   "subFields":[
+                                      
+                                   ]
+                                }
+                             ]
+                          },
+                          {
+                             "name":"application-checks",
+                             "value":null,
+                             "subFields":[
+                                {
+                                   "name":"heart-beat-check",
+                                   "value":"/probes/liveness",
+                                   "subFields":[
+                                      
+                                   ]
+                                },
+                                {
+                                   "name":"conscious-check",
+                                   "value":"/probes/readyness",
+                                   "subFields":[
+                                      
+                                   ]
+                                },
+                                {
+                                   "name":"birth-check",
+                                   "value":"/probes/startup",
+                                   "subFields":[
+                                      
+                                   ]
+                                }
+                             ]
+                          }
+                       ]
+                    }
+                """.trimIndent().replace(Regex("[\t\r\n]+"), "").replace(Regex(" {2,}"), "")
             )
         }
     }
